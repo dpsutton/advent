dada0b599f7b81f56e9b7c48d4ffdb0a9c456166
dan sutton <dan@localhost.localdomain>
2017-12-04 01:19:26 -0600

    day 3 docstrings and comments

--- src/advent/day3.clj
+++ src/advent/day3.clj
@@ -59,37 +59,59 @@
             side       (elements-per-side ring)]
         (partition side (dec side) (cons high (range low (inc high)))))))
 
-(defn sides [n])
-
-(defn relevant-sides [n]
+(defn relevant-sides
+  "Given a cell, finds the side that contains it and the inner side that
+  must be considered to find all adjacent sides"
+  [n]
   (if (= n 1) [[1] []]
-      (let [ring  (which-ring rings-and-corners n)
-            paired-sides (map vector (sides-for-ring ring) (sides-for-ring (dec ring)))]
+      (let [ring         (which-ring rings-and-corners n)
+            paired-sides (map vector
+                              ;; pair right/top/left/middle sides of
+                              ;; the outer ring with the matching
+                              ;; sides from the inner ring
+                              (sides-for-ring ring)
+                              (sides-for-ring (dec ring)))]
         (some (fn [[outer inner]] (when (some #{n} outer)
-                                  [outer inner]))
+                                    [outer inner]))
               paired-sides))))
 
 (defn inner-touching [n]
   (let [[outer inner]  (relevant-sides n)
         pad            [:none :none]
+        ;; pad and partition so that first cell is adjacent to [:none
+        ;; :none corner], second adjacent to [:none corner first]
+        ;; ... etc. This way all cells are adjacent to three to make
+        ;; the partition easier
         inner          (concat pad inner pad)
         touching-pairs (map vector outer (partition 3 1 inner))]
-    (some (fn [[cell adjacent]] (when (= cell n)
-                                  (distinct (cons (dec n) (remove #{:none} adjacent)))))
+    (some (fn [[cell adjacent]]
+            (when (= cell n)
+              ;; include the previous cell as well as its possible its
+              ;; in the inner ring ie, cell 10 begins a new ring and
+              ;; its previous cell 9 is adjacent in the inner ring
+              (distinct (cons (dec n) (remove #{:none} adjacent)))))
           touching-pairs)))
 
 (defn outer-touching [n]
   (if (= n 1)
+    ;; no other cells in the first ring of cell 1
     []
     (let [ring               (which-ring rings-and-corners n)
           sides              (sides-for-ring ring)
+          ;; if the second on a side it touches cell (- n 2) but the
+          ;; first such corner does not (that cell is on an inside ring)
           near-corner        (map second (drop 1 sides))
           touching-in-corner (when (some #{n} near-corner)
                                [(- n 2)])
+          ;; the last two cells are diagonal to or below the first
+          ;; cell in the ring
           touches-first      (let [last-side (last sides)
                                    last-two (take 2 (reverse last-side))]
                                (when (some #{n} last-two)
                                  [(second (first sides))]))]
+      ;; include the previous cell (usually in the outer ring with
+      ;; it), those it is touching diagonally backwards and possible
+      ;; the first cell if it touches it
       (concat [(dec n)] touching-in-corner touches-first))))
 
 (defn cell-value [n]
@@ -98,6 +120,8 @@
     (let [inner (inner-touching n)
           outer (outer-touching n)
           touching (distinct (concat inner outer))]
+      ;; so a cell's value is the sum of the values it is touching,
+      ;; either in its own ring or in the inner ring
       (apply + (map cell-value touching)))))
 
 (deftest cell-value-tests
