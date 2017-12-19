29be93756bd4e94d6bbb7226d0b38d1f70c595aa
dan sutton <danielsutton01@gmail.com>
2017-12-18 23:48:32 -0600

    day 11 explanations

--- src/advent/day11.clj
+++ src/advent/day11.clj
@@ -3,10 +3,19 @@
             [advent.day11data :as data]
             [clojure.test :refer [deftest is are testing]]))
 
+;;; Free (Abelian) Group
+;; a free group is just a list of characters along with a few
+;; relations over those characters. We are taking the free group over
+;; :n :s :ne :nw :se :sw.
+
+;; we know the inverses of each element is just "back where you came
+;; from"
 (def equivalences {[:ne :sw] []
                    [:n :s]   []
                    [:nw :se] []})
 
+;; and we can simplify a few things like moving south east and then
+;; north is just moving north east
 (def simplifications {[:se :n]  [:ne]
                       [:sw :n]  [:nw]
                       [:s :ne]  [:se]
@@ -14,6 +23,9 @@
                       [:sw :se] [:s]
                       [:nw :s]  [:sw] })
 
+;; since we are in an abelian free group, it doesn't matter what order
+;; the symbols are in. If we find a pair that can collapse to the one
+;; or no symbols, we will apply that transformation
 (defn has-pair [input [x y]]
   (when (and (some #{x} input)
              (some #{y} input))
@@ -28,6 +40,13 @@
         without-both (without without-x y)]
     (concat without-both replacement)))
 
+;; we actually don't need to worry about fixed points since we do the
+;; simplifications first and then the equivalences. In other words, we
+;; collapse two elements into one as much as we can. And then we loop
+;; over the collection removing all annihilating pairs (eg, north and
+;; south). Once this pass has been done there can be no more
+;; optimizations as we do not introduce any other symbols that could
+;; create new relations
 (defn iterate-to-fixed-point
   [input relations]
   (let [r (keys relations)]
@@ -36,11 +55,15 @@
         (recur (replace-pair bag reduction (get relations reduction)))
         bag))))
 
+;; so we apply all simplifications and then combine all annihilators.
 (defn simplified-path [path]
   (-> path
       (iterate-to-fixed-point simplifications)
       (iterate-to-fixed-point equivalences)))
 
+;; since we have simplified the path to only "meaningful" movements,
+;; the distance we are aware is simply the length of the path after
+;; all simplifications
 (defn solve1
   ([] (solve1 data/data))
   ([directions]
