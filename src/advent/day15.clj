1957d6dad8ae2a2f88ff802b69f4347087bcb467
dan sutton <danielsutton01@gmail.com>
2017-12-23 14:17:26 -0600

    day 15

---
+++ src/advent/day15.clj
@@ -0,0 +1,73 @@
+(ns advent.day15
+  (:require [clojure.string :as str]
+            #_[advent.day15data :as data]
+            [clojure.test :refer [deftest is are testing]]
+            [clojure.set :as set]))
+
+;; Integer/MAX_VALUE
+(def divisor 2147483647)
+
+(let [low-16-bitmask (int (dec (Math/pow 2 16)))]
+  (defn lower-16-bits [n]
+    (bit-and n low-16-bitmask)))
+
+(defn match? [x y]
+  (= (lower-16-bits x)
+     (lower-16-bits y)))
+
+(def million (int 1e6))
+
+(defn multiple-of [n]
+  (fn [x]
+    (= 0 (mod x n))))
+
+(defn generations [factor seed multiples-of]
+  (let [matching? (multiple-of multiples-of)]
+    (->> seed
+         (iterate (fn [previous]
+                    (rem (* previous factor)
+                         divisor)))
+         (drop 1)
+         (filter matching?))))
+
+(def sample-generator-a {:factor 16807
+                         :seed 65
+                         :multiples-of 4})
+(def sample-generator-b {:factor 48271
+                         :seed 8921
+                         :multiples-of 8})
+
+(def generator-a {:factor 16807
+                  :seed 873
+                  :multiples-of 4})
+(def generator-b {:factor 48271
+                  :seed 583
+                  :multiples-of 8})
+
+(defn solve
+  [g1 g2 iterations]
+  (let [{seed-a         :seed
+         factor-a       :factor
+         multiples-of-a :multiples-of} g1
+        {seed-b         :seed
+         factor-b       :factor
+         multiples-of-b :multiples-of} g2]
+    (reduce (fn [match-count [gen1 gen2]]
+              (if (match? gen1 gen2)
+                (inc match-count)
+                match-count))
+            0
+            (map vector
+                 (take iterations (generations factor-a seed-a multiples-of-a))
+                 (take iterations (generations factor-b seed-b multiples-of-b))))))
+
+(defn solve1
+  ([] (solve1 generator-a generator-b (* 40 million)))
+  ([ga gb iterations]
+   (solve (assoc ga :multiples-of 1)
+          (assoc gb :multiples-of 1)
+          iterations)))
+
+(defn solve2
+  ([] (solve2 generator-a generator-b (* 5 million)))
+  ([ga gb iterations] (solve ga gb iterations)))
