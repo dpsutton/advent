503fcf6f6bda76d5972eba7c3548ff3c3107fff7
dan sutton <dan@dpsutton.com>
2019-12-02 20:10:14 -0600

    Day 02
    No need to brute force it. The system only has addition and
    multiplication in it so the output is pretty determined. Just find the
    coarse scale and the fine scale (which turned out to be 1) and then
    find the combinations that add up to the goal.
    
    Price is right rules for the coarse and then make up the difference
    with the other. Would have been perhaps more difficult if they were
    relatively prime or the fine adjustment was a bit larger than one.

---
+++ 2019/src/advent/02.clj
@@ -0,0 +1,52 @@
+(ns advent.02
+  (:require
+   [clojure.java.io :as io]
+   [clojure.string :as str]))
+
+(def input
+  (let [f #(->> (str/split % #",")
+                (map (fn [x] (Long/parseLong x))))]
+    (delay
+      (with-open [in (io/reader "resources/02.txt")]
+        (into [] (mapcat f (line-seq in)))))))
+
+(defn interpret
+  [instructions]
+  (let [ins-at (fn [instructions couplet]
+                 (->> instructions (drop (* 4 couplet)) (take 4)))]
+    (loop [couplet 0
+           state instructions]
+      (let [[opcode a b r] (ins-at state couplet)]
+        (if (= opcode 99)
+          state
+          (let [va (get state a)
+                vb (get state b)
+                op (case opcode 1 + 2 *)]
+            (recur (inc couplet)
+                   (assoc state r (op va vb)))))))))
+
+(defn solve-1
+  ([]
+   (solve-1 (assoc @input 1 12 2 2)))
+  ([instructions]
+   (first (interpret instructions))))
+
+(defn run-with
+  ([instructions noun verb]
+   (solve-1 (assoc instructions 1 noun 2 verb))))
+
+(defn solve-2
+  []
+  (let [coarse (- (run-with @input 2 1) (run-with @input 1 1))
+        goal   19690720
+        base   (run-with @input 0 0)]
+    (let [noun (quot (- goal base) coarse)
+          verb (- goal (+ base (* noun coarse)))]
+      [noun verb])))
+
+(comment
+  (interpret [1,9,10,3,2,3,11,0,99,30,40,50])
+  (interpret [1,0,0,0,99])
+  (interpret [2,3,0,3,99])
+  (interpret [2,4,4,5,99,0])
+  (interpret [1,1,1,4,99,5,6,0,99]))
